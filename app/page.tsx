f74a3174987c9d7dd4e657bc930fd4803002ac1a
rafaeelricco <rafaelricco10@gmail.com>
2023-09-16 19:07:22 -0300

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+   return (
+      <main className="container">
+         <h1 className="font-semibold text-4xl py-6">deScier Plataform</h1>
+      </main>
+   )
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,39 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+   content: ['./pages/**/*.{js,ts,jsx,tsx,mdx}', './components/**/*.{js,ts,jsx,tsx,mdx}', './app/**/*.{js,ts,jsx,tsx,mdx}'],
+   theme: {
+      container: {
+         center: true,
+         padding: '2rem',
+         screens: {
+            '2xl': '1400px'
+         }
+      },
+      screens: {
+         mobile: { max: '375px' },
+         tablet: { max: '640px' },
+         laptop: { max: '1024px' },
+         desktop: { max: '1280px' }
+      },
+      colors: {},
+      extend: {
+         keyframes: {
+            'accordion-down': {
+               from: { height: '0' },
+               to: { height: 'var(--radix-accordion-content-height)' }
+            },
+            'accordion-up': {
+               from: { height: 'var(--radix-accordion-content-height)' },
+               to: { height: '0' }
+            }
+         },
+         animation: {
+            'accordion-down': 'accordion-down 0.2s ease-out',
+            'accordion-up': 'accordion-up 0.2s ease-out'
+         }
+      }
+   },
+   plugins: [require('tailwindcss-animate')]
+}
+export default config
