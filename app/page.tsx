e09323d5b85d5c8403e0b0c384ce9e8d2bafc881
rafaeelricco <rafaelricco10@gmail.com>
2023-09-18 17:23:40 -0300

    intial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
    return (
       <main className="container">
-         <h1 className="font-semibold text-4xl py-6">deScier Plataform</h1>
+         <h1 className="font-semibold text-4xl">deScier Plataform</h1>
       </main>
    )
 }
