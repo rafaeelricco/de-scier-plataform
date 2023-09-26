8b5a25663ef12ea66ff3698056e340358489641d
Rafael Ricco <rafaelricco10@gmail.com>
2023-09-26 20:46:48 -0300

    feat(title-page-component): created page title component for easy reuse

---
+++ components/common/Title/Page.tsx
@@ -0,0 +1,68 @@
+import React from 'react'
+import { twMerge } from 'tailwind-merge'
+
+/**
+ * @description Root Component.
+ * Root is a reusable wrapper component that accepts child elements.
+ * It styles its children with specific properties (grid layout, gap, flow direction, alignment)
+ * and then renders them.
+ *
+ * @param {React.ReactNode} children - Child elements to be rendered within the Root component.
+ *
+ */
+const Root: React.FC<{
+   children: React.ReactNode
+   className?: string
+}> = ({ children, className }: { children: React.ReactNode; className?: string }) => {
+   return (
+      <React.Fragment>
+         <div
+            className={twMerge(
+               'grid gap-4 grid-flow-col justify-start items-center mt-16 mb-8',
+               className
+            )}
+         >
+            {children}
+         </div>
+      </React.Fragment>
+   )
+}
+
+/**
+ * @description Title Component.
+ * Title is a reusable component that accepts child elements.
+ * It styles its children with specific properties (font size and weight)
+ * and then renders them within an h1 tag.
+ *
+ * @param {React.ReactNode} children - Child elements to be rendered within the Title component.
+ *
+ */
+const Title: React.FC<{ children: React.ReactNode }> = ({
+   children
+}: {
+   children: React.ReactNode
+}) => {
+   return (
+      <React.Fragment>
+         <h1 className="text-2xl font-semibold">{children}</h1>
+      </React.Fragment>
+   )
+}
+
+/**
+ * @description Icon Component.
+ * Icon is a reusable component that accepts child elements.
+ * It doesn't add any styles, but provides a way to group icon elements for better semantics.
+ *
+ * @param {React.ReactNode} children - Child elements to be rendered within the Icon component.
+ *
+ */
+const Icon: React.FC<{ children: React.ReactNode }> = ({
+   children
+}: {
+   children: React.ReactNode
+}) => {
+   return <React.Fragment>{children} </React.Fragment>
+}
+
+export { Icon, Root, Title }
