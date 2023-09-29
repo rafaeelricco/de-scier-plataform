b3759e9be233e644d8ec16c310c0e485f416d352
Rafael Ricco <rafaelricco10@gmail.com>
2023-09-29 13:02:41 -0300

    feat(side-profile): creted intial layout for profile sidebar

--- components/common/Profile/Profile.tsx
+++ components/common/Profile/Profile.tsx
@@ -3,8 +3,11 @@
 import * as Button from '@components/common/Button/Button'
 import Image from 'next/image'
 import { usePathname, useRouter } from 'next/navigation'
+import ShapeDeScierHandBookBottom from 'public/svgs/modules/sidebar/Ellipse 46.svg'
+import ShapeDeScierHandBookTop from 'public/svgs/modules/sidebar/Ellipse 48.svg'
+import IllustrationHandBook from 'public/svgs/modules/sidebar/emojione-v1_document.svg'
 import React from 'react'
-import { PlusCircle } from 'react-bootstrap-icons'
+import { CaretRight, PlusCircle } from 'react-bootstrap-icons'
 import SubmitedItem from './SubmitedItem/SubmitedItem'
 
 const Profile: React.FC = () => {
@@ -41,7 +44,20 @@ const Profile: React.FC = () => {
                      </Button.Button>
                   </div>
                </div>
-               <div className="bg-terciary-main rounded-lg w-full p-4 h-20" />
+               <div className="relative rounded-lg w-full p-4 h-20 gradient-grad-dark overflow-hidden">
+                  <ShapeDeScierHandBookTop className="absolute top-[-0.5rem] left-0" />
+                  <div className="flex gap-2 w-full p-3 items-center absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                     <IllustrationHandBook />
+                     <div className="grid items-center">
+                        <p className="text-sm font-semibold text-[#F4F4F4]">Descier Handbook</p>
+                        <div className="flex justify-between items-center">
+                           <p className="text-[13px] text-[#F4F4F4]">Must-read for researchers!</p>
+                           <CaretRight size={16} className="fill-[#F4F4F4]" />
+                        </div>
+                     </div>
+                  </div>
+                  <ShapeDeScierHandBookBottom className="absolute bottom-[-0.5rem] right-0" />
+               </div>
                <div className="divider-h" />
                <div className="grid gap-4">
                   <p className="text-base font-semibold text-[#3F3F44]">Last Submitted</p>
