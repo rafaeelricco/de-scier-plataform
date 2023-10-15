b664edd73bff46c9542821e889bd797485e239a9
rafaeelricco <rafaelricco10@gmail.com>
2023-10-15 16:04:21 -0300

    feat(header): added login modal to header component

--- components/common/Header/Header.tsx
+++ components/common/Header/Header.tsx
@@ -1,7 +1,11 @@
 'use client'
 
+import ForgotPasswordModal from '@/components/modules/ForgotPassword/ForgotPassword'
+import LoginModal from '@/components/modules/Login/Login'
+import RegisterModal from '@/components/modules/Register/Register'
 import { home_routes } from '@/routes/home'
 import * as Button from '@components/common/Button/Button'
+import * as Dialog from '@components/common/Dialog/Digalog'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import DeScierLogo from 'public/svgs/common/logo/deScier - Logo.svg'
@@ -24,8 +28,40 @@ const Header: React.FC = () => {
          return false
       }
    }
+
+   const login_component = 'login'
+   const register_component = 'register'
+   const forgot_password_component = 'forgot_password'
+
+   const [component, setComponent] = React.useState(login_component)
+   const [open, setOpen] = React.useState(false)
    return (
       <React.Fragment>
+         <Dialog.Root open={open}>
+            <Dialog.Overlay />
+            <Dialog.Content className={twMerge('w-[80%] max-w-[1200px] p-0', component === forgot_password_component && 'max-w-[500px]')}>
+               {component === login_component && (
+                  <LoginModal
+                     onClose={() => setOpen(false)}
+                     onForgotPassword={() => setComponent(forgot_password_component)}
+                     onLogin={() => setComponent(login_component)}
+                     onRegister={() => setComponent(register_component)}
+                  />
+               )}
+               {component === register_component && (
+                  <RegisterModal
+                     onBack={() => setComponent(login_component)}
+                     onClose={() => console.log('close event')}
+                     onLogin={() => setComponent(login_component)}
+                     onRegister={() => setComponent(register_component)}
+                     onReturnToLogin={() => setComponent(login_component)}
+                  />
+               )}
+               {component === forgot_password_component && (
+                  <ForgotPasswordModal onBack={() => setComponent(login_component)} onClose={() => setComponent(login_component)} />
+               )}
+            </Dialog.Content>
+         </Dialog.Root>
          <div className="py-3 px-8 bg-white flex items-center gap-8 rounded-full shadow-search w-fit justify-center">
             <div className="flex items-center gap-8">
                <DeScierLogo className="w-10 h-fit" />
@@ -46,7 +82,7 @@ const Header: React.FC = () => {
                </div>
             </div>
             <div className="flex items-center gap-4">
-               <Button.Button variant="outline" className="rounded-full py-2 px-8 text-sm">
+               <Button.Button variant="outline" className="rounded-full py-2 px-8 text-sm" onClick={() => setOpen(true)}>
                   Login
                </Button.Button>
                <Button.Button variant="primary" className="rounded-full py-2 px-8 text-sm">
