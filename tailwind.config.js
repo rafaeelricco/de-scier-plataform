2f1ab2b395e9dea2a49de6710d13f61d60f86745
rafaeelricco <rafaelricco10@gmail.com>
2023-10-22 10:23:33 -0300

    fix: material tailwindcss config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,14 @@ const withMT = require('@material-tailwind/react/utils/withMT')
 
 module.exports = withMT({
    darkMode: ['class'],
-   content: ['./pages/**/*.{ts,tsx}', './components/**/*.{ts,tsx}', './app/**/*.{ts,tsx}', './src/**/*.{ts,tsx}'],
+   content: [
+      './pages/**/*.{ts,tsx}',
+      './components/**/*.{ts,tsx}',
+      './app/**/*.{ts,tsx}',
+      './src/**/*.{ts,tsx}',
+      './node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}',
+      './node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}'
+   ],
    theme: {
       container: {
          center: true,
