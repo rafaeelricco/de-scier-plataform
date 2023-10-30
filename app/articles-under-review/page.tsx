b405fb41fd541cc742754e375ced303d03c6eed9
rafaeelricco <rafaelricco10@gmail.com>
2023-10-29 22:51:30 -0300

    feat(my-articles-under-review): implemented search bar

--- app/articles-under-review/page.tsx
+++ app/articles-under-review/page.tsx
@@ -3,6 +3,7 @@
 import { Dropdown } from '@/components/common/Dropdown/Dropdown'
 import PaginationComponent from '@/components/common/Pagination/Pagination'
 import { ArticleUnderReview, ArticleUnderReviewProps, ArticleUnderReviewSkeleton } from '@/components/common/Publication/Item/ArticlesUnderReview'
+import useDebounce from '@/hooks/useDebounce'
 import { document_types } from '@/mock/document_types'
 import { filter_status } from '@/mock/dropdow_filter_options'
 import { home_routes } from '@/routes/home'
@@ -18,34 +19,28 @@ export default function ArticlesUnderReviewPage() {
     */
    const { articles, loading } = useArticles()
 
-   /**
-    * @dev Number of articles displayed per page.
-    */
+   /** @dev Number of articles displayed per page. */
    const per_page = 8
 
-   /**
-    * @notice Current page number state.
-    */
+   /** @notice Current page number state.*/
    const [page, setPage] = React.useState(1)
 
-   /**
-    * @notice State for the selected document type filter.
-    */
+   /** @notice State for the selected document type filter. */
    const [documentType, setDocumentType] = React.useState<string | null>(null)
 
-   /**
-    * @notice State for the selected status filter.
-    */
+   /** @notice State for the search term. */
+   const [searchTerm, setSearchTerm] = React.useState('')
+
+   /** @notice Debounces the search term. */
+   const debouncedSearchTerm = useDebounce(searchTerm, 500)
+
+   /** @notice State for the selected status filter. */
    const [status, setStatus] = React.useState<string | null>(null)
 
-   /**
-    * @notice Holds the list of filtered articles to be displayed.
-    */
+   /** @notice Holds the list of filtered articles to be displayed. */
    const [results, setResults] = React.useState<ArticleUnderReviewProps[]>([])
 
-   /**
-    * @notice Holds the total number of pages based on the number of results and articles per page.
-    */
+   /** @notice Holds the total number of pages based on the number of results and articles per page. */
    const [totalPages, setTotalPages] = React.useState(Math.ceil(results.length / per_page))
 
    /**
@@ -57,9 +52,12 @@ export default function ArticlesUnderReviewPage() {
    }, [articles])
 
    /**
-    * @notice Filters articles based on selected document type and status.
-    * @dev This effect listens for changes in the articles, documentType, and status states
-    * and updates the results with the filtered list of articles.
+    * @notice Filters articles based on selected document type, status, and title search.
+    * @dev This effect listens for changes in the articles, documentType, status, and debouncedSearchTerm states
+    * and updates the results with the filtered list of articles. It filters articles based on the following criteria:
+    * - Document Type: If a document type is selected, only articles with a matching document type will be displayed.
+    * - Status: If a status is selected, only articles with a matching status will be displayed.
+    * - Title Search: If a search term is provided, only articles with titles containing the search term (case-insensitive) will be displayed.
     */
    React.useEffect(() => {
       if (!articles) return
@@ -74,8 +72,12 @@ export default function ArticlesUnderReviewPage() {
          filteredArticles = filteredArticles.filter((article) => article.status?.toLocaleLowerCase() == status?.toLowerCase())
       }
 
+      if (debouncedSearchTerm) {
+         filteredArticles = filteredArticles.filter((article) => article.title.toLowerCase().includes(debouncedSearchTerm.toLowerCase()))
+      }
+
       setResults(filteredArticles)
-   }, [articles, documentType, status])
+   }, [articles, documentType, status, debouncedSearchTerm])
 
    /**
     * @notice Recalculates the total number of pages whenever the list of results changes.
@@ -93,7 +95,7 @@ export default function ArticlesUnderReviewPage() {
          <div className="grid gap-6">
             <div className="grid gap-6">
                <div className="flex items-center gap-2">
-                  <Input.Search placeholder="Find articles with this terms" />
+                  <Input.Search placeholder="Find articles with this terms" onChange={(e) => setSearchTerm(e.target.value)} />
                </div>
                <div className="flex flex-col md:flex-row md:items-center gap-2">
                   <Dropdown items={document_types} label="Order by:" onSelect={(value) => setDocumentType(value)} />
