bcfc4ca763e6b696d2cec4283296dfec78ac5061
rafaeelricco <rafaelricco10@gmail.com>
2023-11-16 13:13:57 -0300

    chore: removed comment

--- utils/error_message.ts
+++ utils/error_message.ts
@@ -2,6 +2,5 @@ export function ErrorMessage({ error, message }: { error: any; message: string }
    if (error == 'invalid_type' || error == 'too_small') {
       return message
    }
-
    return null
 }
