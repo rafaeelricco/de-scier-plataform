8413682b88d7eefa64d9f3437b77e001bfb3adb6
rafaeelricco <rafaelricco10@gmail.com>
2023-11-19 11:19:24 -0300

    fix(summary): added hover card to titles on published and under review

--- components/common/Publication/Item/InReview.tsx
+++ components/common/Publication/Item/InReview.tsx
@@ -1,6 +1,66 @@
+import { HoverCard, HoverCardContent, HoverCardTrigger } from '@/components/ui/hover-card'
+import useDimension from '@/hooks/useWindowDimension'
 import Image from 'next/image'
 import React from 'react'
 import './Item.css'
+
+const InReviewItem: React.FC<InReviewItemProps> = ({ date, link, ready_to_publish, status_editor, status_reviewer, image, title }: InReviewItemProps) => {
+   const { windowDimension } = useDimension()
+   const isSmallDesktop = windowDimension && windowDimension < 1440 && windowDimension >= 1024
+   return (
+      <React.Fragment>
+         <div className="grid md:grid-cols-max-auto md:justify-start items-center gap-4">
+            <Image src={image} width={420} height={80} alt={title} style={{ objectFit: 'cover' }} className="rounded-md h-20 w-full object-cover" />
+            <div className="grid gap-2">
+               {isSmallDesktop ? (
+                  <HoverCard>
+                     <HoverCardTrigger className="truncate cursor-pointer">
+                        <h6 className="text-sm md:text-base font-semibold text-secundary_blue-main truncate cursor-pointer">{title}</h6>
+                     </HoverCardTrigger>
+                     <HoverCardContent>{title}</HoverCardContent>
+                  </HoverCard>
+               ) : (
+                  <h6 className="text-sm md:text-base font-semibold text-secundary_blue-main truncate cursor-pointer">{title}</h6>
+               )}
+               <div className="flex flex-col xxl:flex-row xxl:items-center md:justify-start gap-2">
+                  <div className="flex flex-grow justify-center gap-1 items-center border p-1 rounded-md w-full md:border-none md:p-0 md:justify-start md:w-fit md:flex-grow-0">
+                     {ready_to_publish ? (
+                        <p className="text-xs sm:text-base font-semibold text-status-green truncate xl:max-w-[6ch] 2xl:max-w-full">Ready to publish</p>
+                     ) : (
+                        <>
+                           {status_editor == 'approved' && (
+                              <p className="text-xs sm:text-base font-semibold text-status-green truncate xl:max-w-[6ch] 2xl:max-w-full">
+                                 Approved by editor
+                              </p>
+                           )}
+                           {status_editor == 'pending' && (
+                              <p className="text-xs sm:text-base font-semibold text-status-pending truncate xl:max-w-[6ch] 2xl:max-w-full">
+                                 Editor pending
+                              </p>
+                           )}
+                           <span className="text-xs sm:text-sm font-semibold text-neutral-light_gray truncate xl:max-w-[6ch] 2xl:max-w-full">/</span>
+                           {status_reviewer == 'approved' && (
+                              <p className="text-xs sm:text-base font-semibold text-status-green truncate xl:max-w-[6ch] 2xl:max-w-full">
+                                 Reviewer approval
+                              </p>
+                           )}
+                           {status_reviewer == 'pending' && (
+                              <p className="text-xs sm:text-base font-semibold text-status-pending truncate xl:max-w-[6ch] 2xl:max-w-full">
+                                 Reviewer pending
+                              </p>
+                           )}
+                        </>
+                     )}
+                  </div>
+                  <span className="hidden xxl:block text-neutral-light_gray">•</span>
+                  <p className="text-xs sm:text-sm md:hidden text-center text-neutral-gray">Under review since {date}</p>
+                  <p className="hidden md:block text-sm sm:text-base text-neutral-gray">{date}</p>
+               </div>
+            </div>
+         </div>
+      </React.Fragment>
+   )
+}
 
 export interface InReviewItemProps {
    id?: string
@@ -11,37 +71,6 @@ export interface InReviewItemProps {
    date: string
    title: string
    link: string
-}
-
-const InReviewItem: React.FC<InReviewItemProps> = ({ date, link, ready_to_publish, status_editor, status_reviewer, image, title }: InReviewItemProps) => {
-   return (
-      <React.Fragment>
-         <div className="grid md:grid-cols-max-auto md:justify-start items-center gap-4">
-            <Image src={image} width={420} height={80} alt={title} style={{ objectFit: 'cover' }} className="rounded-md h-20 w-full object-cover" />
-            <div className="grid gap-2">
-               <h6 className="text-sm md:text-base font-semibold text-secundary_blue-main">{title}</h6>
-               <div className="flex flex-col md:flex-row items-center md:justify-start gap-2">
-                  <div className="flex flex-grow justify-center gap-1 items-center border p-1 rounded-md w-full md:border-none md:p-0 md:justify-start md:w-fit md:flex-grow-0">
-                     {ready_to_publish ? (
-                        <p className="text-xs sm:text-base font-semibold text-status-green">Ready to publish</p>
-                     ) : (
-                        <>
-                           {status_editor == 'approved' && <p className="text-xs sm:text-base font-semibold text-status-green">Approved by editor</p>}
-                           {status_editor == 'pending' && <p className="text-xs sm:text-base font-semibold text-status-pending">Editor pending</p>}
-                           <span className="text-xs sm:text-sm font-semibold text-neutral-light_gray">/</span>
-                           {status_reviewer == 'approved' && <p className="text-xs sm:text-base font-semibold text-status-green">Reviewer approval</p>}
-                           {status_reviewer == 'pending' && <p className="text-xs sm:text-base font-semibold text-status-pending">Reviewer pending</p>}
-                        </>
-                     )}
-                  </div>
-                  <span className="hidden md:block text-neutral-light_gray">•</span>
-                  <p className="text-xs sm:text-sm md:hidden text-center text-neutral-gray">Under review since {date}</p>
-                  <p className="hidden md:block text-sm sm:text-base text-neutral-gray">{date}</p>
-               </div>
-            </div>
-         </div>
-      </React.Fragment>
-   )
 }
 
 export default InReviewItem
--- components/common/Publication/Item/Published.tsx
+++ components/common/Publication/Item/Published.tsx
@@ -1,3 +1,5 @@
+import { HoverCard, HoverCardContent, HoverCardTrigger } from '@/components/ui/hover-card'
+import useDimension from '@/hooks/useWindowDimension'
 import Image from 'next/image'
 import React from 'react'
 import { Eye, HandThumbsUpFill } from 'react-bootstrap-icons'
@@ -13,6 +15,8 @@ export interface PublicationItemProps {
 }
 
 const PublicationItem: React.FC<PublicationItemProps> = ({ date, link, likes, views, id, title, image }: PublicationItemProps) => {
+   const { windowDimension } = useDimension()
+   const isSmallDesktop = windowDimension && windowDimension < 1440 && windowDimension >= 1024
    return (
       <React.Fragment>
          <div className="flex flex-col lg:flex-row lg:items-center lg:gap-4 gap-2">
@@ -25,7 +29,16 @@ const PublicationItem: React.FC<PublicationItemProps> = ({ date, link, likes, vi
                className="rounded-md w-full h-20 lg:w-20 object-cover"
             />
             <div className="grid gap-2">
-               <h6 className="text-sm md:text-base font-semibold text-secundary_blue-main truncate">{title}</h6>
+               {isSmallDesktop ? (
+                  <HoverCard>
+                     <HoverCardTrigger className="truncate cursor-pointer">
+                        <h6 className="text-sm md:text-base font-semibold text-secundary_blue-main truncate cursor-pointer">{title}</h6>
+                     </HoverCardTrigger>
+                     <HoverCardContent>{title}</HoverCardContent>
+                  </HoverCard>
+               ) : (
+                  <h6 className="text-sm md:text-base font-semibold text-secundary_blue-main truncate cursor-pointer">{title}</h6>
+               )}
                <div className="flex flex-wrap gap-2 items-center">
                   <div className="flex items-center gap-1">
                      <HandThumbsUpFill className="text-terciary-main lg:w-4 2xl:w-5" />
@@ -36,7 +49,7 @@ const PublicationItem: React.FC<PublicationItemProps> = ({ date, link, likes, vi
                      <Eye className="text-terciary-main lg:w-4 2xl:w-5" />
                      <p className="text-sm md:text-base text-neutral-gray">{views}</p>
                   </div>
-                  <span className="text-neutral-gray">•</span>
+                  <span className="text-neutral-gray md:hidden lg:hidden xxl:hidden 2xl:block">•</span>
                   <p className="text-sm md:text-base text-neutral-gray truncate">{date}</p>
                </div>
             </div>
