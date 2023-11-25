af5b2e2fff68c4b622c959d61e26be6dddfba376
rafaeelricco <rafaelricco10@gmail.com>
2023-11-25 18:00:54 -0300

    chore(building-warnings): updated dependencies to avoid warnings

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
    webpack: (config, { isServer }) => {
-      if (!isServer) {
-         config.resolve.fallback = {
-            fs: false
-         }
-      }
-      // Grab the existing rule that handles SVG imports
       const fileLoaderRule = config.module.rules.find((rule) => rule.test?.test?.('.svg'))
 
       config.module.rules.push(
