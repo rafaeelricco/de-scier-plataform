c2902efb6bdbc6f871be6308d9470833e46a393a
rafaeelricco <rafaelricco10@gmail.com>
2023-12-03 14:25:50 -0300

    fix: hover underline in ArticleCard and add link to title

--- components/modules/Summary/TopPapers/TopPapers.tsx
+++ components/modules/Summary/TopPapers/TopPapers.tsx
@@ -12,7 +12,7 @@ const TopPapers: React.FC<TopPapersProps> = ({ publishedDocuments }: TopPapersPr
       <React.Fragment>
          <div className="grid gap-4">
             <h3 className="text-xl font-[500] lg:text-lg 2xl:text-xl">My top papers</h3>
-            <div className="grid gap-4 max-h-[13rem] overflow-y-scroll min-h-[188px]">
+            <div className="grid gap-4 max-h-[13rem] overflow-y-scroll min-h-[188px] content-start">
                {publishedDocuments.length === 0 ? (
                   <p className="text-gray-500 text-center flex items-center justify-center">No papers found.</p>
                ) : (
